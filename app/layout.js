d434bb580d33a359b757eb6773ddbb20481e380f
Marc Lou <marc.louvion@gmail.com>
2024-10-06 15:35:03 +0800

    fix: remove Plausible

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import PlausibleProvider from "next-plausible";
 import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
@@ -8,10 +7,10 @@ import "./globals.css";
 const font = Inter({ subsets: ["latin"] });
 
 export const viewport = {
-  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
-  themeColor: config.colors.main,
-  width: "device-width",
-  initialScale: 1,
+	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+	themeColor: config.colors.main,
+	width: "device-width",
+	initialScale: 1,
 };
 
 // This adds default SEO tags to all pages in our app.
@@ -19,17 +18,16 @@ export const viewport = {
 export const metadata = getSEOTags();
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" data-theme={config.colors.theme} className={font.className}>
-      {config.domainName && (
-        <head>
-          <PlausibleProvider domain={config.domainName} />
-        </head>
-      )}
-      <body>
-        {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
-        <ClientLayout>{children}</ClientLayout>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			data-theme={config.colors.theme}
+			className={font.className}
+		>
+			<body>
+				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
+				<ClientLayout>{children}</ClientLayout>
+			</body>
+		</html>
+	);
 }
