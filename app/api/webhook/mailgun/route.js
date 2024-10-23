190f36e3fa6caf34c3b43e0018cc6c0ec3e9209d
Wahab Shaikh <hey.wahabshaikh@gmail.com>
2024-10-23 12:21:04 +0530

    fix: use mailgun signing key

--- app/api/webhook/mailgun/route.js
+++ app/api/webhook/mailgun/route.js
@@ -7,16 +7,18 @@ import crypto from "crypto";
 // See more: https://shipfa.st/docs/features/emails
 export async function POST(req) {
   try {
-    // extract the email content, subject and sender
     const formData = await req.formData();
 
-    const timestamp = formData.get("timestamp");
-    const token = formData.get("token");
-    const signature = formData.get("signature");
+    // Get your HTTP webhook signing key from https://app.mailgun.com/mg/sending/mg.<yourdomain>/webhooks and add it to .env.local
+    const signingKey = process.env.MAILGUN_SIGNING_KEY;
+
+    const timestamp = formData.get("timestamp").toString();
+    const token = formData.get("token").toString();
+    const signature = formData.get("signature").toString();
 
     const value = timestamp + token;
     const hash = crypto
-      .createHmac("sha256", process.env.MAILGUN_API_KEY)
+      .createHmac("sha256", signingKey)
       .update(value)
       .digest("hex");
 
@@ -24,6 +26,7 @@ export async function POST(req) {
       return NextResponse.json({ error: "Invalid signature" }, { status: 401 });
     }
 
+    // extract the sender, subject and email content
     const sender = formData.get("From");
     const subject = formData.get("Subject");
     const html = formData.get("body-html");
